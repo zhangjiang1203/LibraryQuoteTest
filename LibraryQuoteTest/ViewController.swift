52368b46e4a25dac3186aa46222e9327f16e3c47
zhangjiang <896884553@qq.com>
2016-01-06 16:40:23 +0800

    Initial Commit

---
+++ LibraryQuoteTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LibraryQuoteTest
+//
+//  Created by zjhaha on 16/1/6.
+//  Copyright © 2016年 zjhaha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
